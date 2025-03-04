4584b3e7162ff3b6db296236cf687363753849af
vectorwang <vectorwang@hotmail.com>
2025-03-04 19:50:41 +0800

    Changed main.py to command line prog; Updated example output

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import os.path
 import json
 import geopandas as gpd
 import matplotlib.pyplot as plt
+import argparse
 
 from typing import Tuple
 from shapely.ops import unary_union
 
 from welzl import welzl
 
-
 config_path = "./config.json"
 
-def parse_s57_file(file_name: str, debug=False) -> Tuple[Tuple[float, float], float]:
+parser = argparse.ArgumentParser()
+parser.add_argument("--output", "-o", type=str, required=True)
+
+def get_s57_file_coverage(file_name: str, debug=False) -> Tuple[Tuple[float, float], float]:
     """
     input:
         file path for s-57 file
@@ -42,13 +45,23 @@ def parse_s57_file(file_name: str, debug=False) -> Tuple[Tuple[float, float], fl
     return min_circle
 
 if __name__ == "__main__":
+    args = parser.parse_args()
+    output_path = args.output
+
+    coverage_data = {}
+
     # fetch paths for all s57 files within source directory
     with open(config_path, "r") as fd:
         s57_source = json.load(fd)["s57_dir"]
 
     s57_file_paths = os.listdir(s57_source)
+    s57_ids = [file_name[:-4] for file_name in s57_file_paths if file_name[-4:] == ".000"]
     s57_file_paths = [os.path.join(s57_source, file_name) for file_name in s57_file_paths]
 
-    for file_name in s57_file_paths:
-        min_circle = parse_s57_file(file_name)
-        print(min_circle)
+    for s57_id, file_name in zip(s57_ids, s57_file_paths):
+        min_circle = get_s57_file_coverage(file_name)
+        coverage_data[s57_id] = min_circle
+    
+    print("Writing parsed s-57 coverage data to: {}".format(output_path))
+    with open(output_path, "w") as fd:
+        fd.write(json.dumps(coverage_data))
